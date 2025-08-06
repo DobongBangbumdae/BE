ec67cead189ce7e445d355f4f3f1e78979178108
hyo-lin <lin1137001@gmail.com>
2025-08-06 22:54:49 +0900

    feat: email과 로그인 타입으로 객체 저장

--- src/main/java/com/dobongzip/dobong/domain/user/repository/UserRepository.java
+++ src/main/java/com/dobongzip/dobong/domain/user/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.dobongzip.dobong.domain.user.repository;
 
 import com.dobongzip.dobong.domain.user.entity.User;
+import com.dobongzip.dobong.global.security.enums.LoginType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByAppId(String appId);
+    boolean existsByEmailAndLoginType(String email, LoginType loginType);
+    Optional<User> findByEmailAndLoginType(String email, LoginType loginType);
+
 }
