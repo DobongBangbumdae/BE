55e6c10dc0e078233444e50688e0e17d77ed3a95
hyo-lin <lin1137001@gmail.com>
2025-08-06 23:01:12 +0900

    feat: email과 로그인타입으로 사용자 반환

--- src/main/java/com/dobongzip/dobong/global/security/dto/request/LoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.dobongzip.dobong.global.security.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class LoginRequestDto {
-    private String appId;
-    private String password;
-}
