883d5e589cb684a649f8e86f87c0db719fc4c079
hyo-lin <lin1137001@gmail.com>
2025-08-07 18:16:08 +0900

    feat: StatusCode 추가

--- src/main/java/com/dobongzip/dobong/global/response/StatusCode.java
+++ src/main/java/com/dobongzip/dobong/global/response/StatusCode.java
@@ -6,13 +6,15 @@ import org.springframework.http.HttpStatus;
 public enum StatusCode {
     SUCCESS(HttpStatus.OK, "SUCCESS200", "SUCCESS"),
 
-    // StatusCode.java
+    // 로그인, 회원가입
     INVALID_PASSWORD_FORMAT(HttpStatus.BAD_REQUEST, "AUTH4001", "비밀번호는 6~20자이며, 영문 대/소문자 및 특수문자 중 2가지 이상을 포함해야 합니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH404", "존재하지 않는 사용자입니다."),
     PROFILE_NOT_COMPLETED(HttpStatus.OK, "AUTH2001", "프로필 정보가 아직 입력되지 않았습니다."),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COMMON4001", "잘못된 요청입니다."),
 
-
+    //회원정보 관리
+    INVALID_CURRENT_PASSWORD(HttpStatus.BAD_REQUEST, "AUTH4001", "현재 비밀번호가 일치하지 않습니다."),
+    PASSWORD_CONFIRM_NOT_MATCH(HttpStatus.BAD_REQUEST, "AUTH4002", "새 비밀번호 확인이 일치하지 않습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH401", "이메일 또는 비밀번호가 일치하지 않습니다."),
     USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "AUTH4002", "이미 등록된 사용자입니다."),
     FAILURE_TEST(HttpStatus.INTERNAL_SERVER_ERROR, "TEST001", "테스트 실패 응답입니다."),
